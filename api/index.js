9b5cc43ac09a48f29433b163327e2cad5cf3eb0d
by柯乐 <1010456736@qq.com>
2024-10-23 09:45:07 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,69 +2,33 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 
-function parseProxyParameters(proxyRequest) {
-  const params = {};
-  // 提取并解码 URL 参数
-  const urlMatch = proxyRequest.url.match(/(?<=[?&])url=(?<url>.*)$/);
-  if (urlMatch) {
-    params.url = decodeURIComponent(urlMatch.groups.url);
-  }
-  return params;
-}
-
 const app = express();
 app.use(cors());
-app.set('json spaces', 2);
 
 app.all('/*', async (req, res) => {
   try {
-    const proxyParams = parseProxyParameters(req);
-    if (!proxyParams.url) {
+    const proxyParams = new URLSearchParams(req.url);
+    const targetUrl = proxyParams.get('url');
+
+    if (!targetUrl) {
       return res.status(400).json({
         title: "CORS代理错误-缺少必需参数",
         detail: "未提供参数：url",
-        tips: "可选域: https://cors.qdqqd.com/?url= , https://cors-qdqqd.vercel.app/?url=",
       });
     }
 
-    // 打印解码后的 URL
-    console.log("Proxying request to:", proxyParams.url);
+    console.log("Proxying request to:", targetUrl);
 
-    // 使用 axios 进行代理请求
-    const response = await axios({
-      method: req.method,
-      url: proxyParams.url,
-      headers: req.headers,  // 复制请求头
-      responseType: 'stream',  // 保持流式响应
-    });
-
-    // 将目标响应的头部和数据传递给客户端
+    const response = await axios.get(targetUrl, { responseType: 'stream' });
     res.set(response.headers);
     response.data.pipe(res);
-    
+
   } catch (err) {
     console.error("API 请求失败:", err.message);
-
-    // 处理不同的错误情况
-    if (err.response) {
-      // 目标服务器返回了非 2xx 的状态码
-      return res.status(err.response.status).json({
-        title: `CORS代理错误-API 返回错误(${err.response.status})`,
-        detail: err.response.data || err.response.statusText,
-      });
-    } else if (err.request) {
-      // 请求已发出，但没有收到回应
-      return res.status(500).json({
-        title: "CORS代理错误-没有收到目标服务器的响应",
-        detail: "请求已发出，但没有收到响应。",
-      });
-    } else {
-      // 设置请求时发生了错误
-      return res.status(500).json({
-        title: "CORS代理错误-请求配置错误",
-        detail: err.message,
-      });
-    }
+    return res.status(500).json({
+      title: "CORS代理错误-API 请求失败",
+      detail: err.message,
+    });
   }
 });
 
